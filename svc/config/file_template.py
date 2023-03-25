7aad6f0ad83a78006c4e468533f3a3a502800ff2
Jon Graf <jgraf@vermeer.com>
2023-03-25 14:02:48 -0500

    created template file to use as a sample when nothing exists

---
+++ svc/config/file_template.py
@@ -0,0 +1,10 @@
+template = {
+    'registered': [
+        {'groupId': 1, 'groupName': 'Living Room', 'brightness': 100, 'on': True, 'lights': [
+            {'brightness': 80, 'lightName': 'lamp', 'lightId': 2, 'groupId': 1, 'on': True},
+            {'brightness': 10, 'lightName': 'desk', 'lightId': 3, 'groupId': 1, 'on': True},
+        ]}],
+    'unregistered': [
+        {'brightness': 100, 'lightName': 'new', 'lightId': 5, 'groupId': None, 'on': True},
+    ]
+}

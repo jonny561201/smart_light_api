8816ae4a22b980a894ef2ff72e78cbe95cb95534
Jon Graf <jongraf1@gmail.com>
2023-03-31 18:23:13 -0500

    updated the light service to call the database to get all lights and map the results as well as initially grab the db groups to set

--- svc/services/light_service.py
+++ svc/services/light_service.py
@@ -1,6 +1,7 @@
 from svc.config.settings_state import Settings
+from svc.repository.light_repository import LightDatabaseManager
 from svc.utils.file_utils import FileUtil
-from svc.utils.tuya_utils import set_switch_brightness
+from svc.utils.tuya_utils import set_switch_brightness, get_light_status
 
 
 def update_light_state(data):
@@ -10,11 +11,23 @@ def update_light_state(data):
 
 
 def get_light_groups():
-    file_util = FileUtil.get_instance()
-    return file_util.get_all_light_groups()
+    with LightDatabaseManager() as db:
+        groups = db.get_light_groups()
+        return list(map(__create_group, groups))
 
 
 def set_light_group(request):
-    settings = Settings.get_instance()
-    file_util = FileUtil().get_instance()
-    file_util.get_json_file(settings.light_file)
+    with LightDatabaseManager() as db:
+        db.get_light_groups()
+
+
+def __create_group(group):
+    return {
+        'groupName': group.name,
+        'groupId': str(group.id),
+        'lights': list(map(__get_light_data, group.devices))
+    }
+
+
+def __get_light_data(light):
+    return get_light_status(light)
--- tests/unit/services/test_light_services.py
+++ tests/unit/services/test_light_services.py
@@ -1,37 +1,46 @@
+import uuid
+
 from mock import patch
 
 from svc.config.settings_state import Settings
+from svc.repository.models.lights import DeviceGroups, Devices
 from svc.services.light_service import get_light_groups, set_light_group
 
 
-@patch('svc.services.light_service.FileUtil')
+@patch('svc.services.light_service.get_light_status')
+@patch('svc.services.light_service.LightDatabaseManager')
 class TestLightServices:
     SETTINGS = None
-    FILE_CONTENTS = [{}]
+    GROUP_ID = str(uuid.uuid4())
+    DEVICE_ID = str(uuid.uuid4())
+    DB_DEVICE = Devices(id=DEVICE_ID, name='Lamp', ip_address='127.0.0.1', local_key='test', type_id='26cf7b35-8366-4c16-be9a-e0009bda62b6')
+    DB_GROUP = DeviceGroups(id=GROUP_ID, name='Bedroom', devices=[DB_DEVICE])
     FILE_NAME = 'test.json'
 
     def setup_method(self):
         self.SETTINGS = Settings.get_instance()
         self.SETTINGS.dev_mode = True
 
-    def test_get_light_groups__should_return_content_from_file(self, file_mock):
-        file_mock.get_instance.return_value.get_all_light_groups.return_value = self.FILE_CONTENTS
+    def test_get_light_groups__should_call_db_for_groups(self, mock_db, mock_status):
+        mock_db.return_value.__enter__.return_value.get_light_groups.return_value = [self.DB_GROUP]
+
+        get_light_groups()
+
+        mock_db.return_value.__enter__.return_value.get_light_groups.assert_called()
+
+    def test_get_light_groups__should_map_db_response(self, mock_db, mock_status):
+        light = {'name': 'Lamp', 'id': self.DEVICE_ID}
+        expected = [{'groupName': 'Bedroom', 'groupId': self.GROUP_ID, 'lights': [light]}]
+        mock_status.return_value = light
+        mock_db.return_value.__enter__.return_value.get_light_groups.return_value = [self.DB_GROUP]
 
         actual = get_light_groups()
 
-        assert actual == self.FILE_CONTENTS
+        assert actual == expected
 
-    def test_get_light_groups__should_return_contents(self, file_mock):
-        test_file = [{'contents': 'im fake!'}]
-        file_mock.get_instance.return_value.get_all_light_groups.return_value = test_file
-
-        actual = get_light_groups()
-
-        assert actual == test_file
-
-    def test_set_light_groups__should_get_file_contents(self, mock_file):
+    def test_set_light_groups__should_get_groups_from_db(self, mock_db, mock_status):
         request = {'groupId': 'abc134', 'on': True}
-        self.SETTINGS.settings = {'lightFile': self.FILE_NAME}
+        mock_db.return_value.__enter__.return_value.get_light_groups.return_value = [self.DB_GROUP]
         set_light_group(request)
 
-        mock_file.get_all_light_groups.assert_called_with(self.FILE_NAME)
+        mock_db.return_value.__enter__.return_value.get_light_groups.assert_called()

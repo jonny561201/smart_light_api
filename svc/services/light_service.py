8e1f1003477c419b5a966240226269c143b51710
Jon Graf <jongraf1@gmail.com>
2023-03-31 19:40:47 -0500

    added the ability to set all the lights in a light group based off request

--- svc/services/light_service.py
+++ svc/services/light_service.py
@@ -1,13 +1,11 @@
-from svc.config.settings_state import Settings
 from svc.repository.light_repository import LightDatabaseManager
-from svc.utils.file_utils import FileUtil
-from svc.utils.tuya_utils import set_switch_brightness, get_light_status
+from svc.utils import tuya_utils
 
 
 def update_light_state(data):
     switch_id = data.get('lightId')
     brightness = data.get('brightness')
-    set_switch_brightness(switch_id, brightness)
+    tuya_utils.set_switch_brightness(switch_id, brightness)
 
 
 def get_light_groups():
@@ -17,17 +15,25 @@ def get_light_groups():
 
 
 def set_light_group(request):
+    on = request.get('on')
+    brightness = request.get('brightness')
+    group_id = request['groupId']
+
     with LightDatabaseManager() as db:
-        db.get_light_groups()
+        lights = db.get_lights_by(group_id)
+        for light in lights:
+            tuya_utils.set_switch(light, on, brightness)
 
 
 def __create_group(group):
+    lights = list(map(__get_light_data, group.devices))
     return {
         'groupName': group.name,
         'groupId': str(group.id),
-        'lights': list(map(__get_light_data, group.devices))
+        'lights': lights,
+        # 'on': lights[0]
     }
 
 
 def __get_light_data(light):
-    return get_light_status(light)
+    return tuya_utils.get_light_status(light)
--- tests/unit/services/test_light_services.py
+++ tests/unit/services/test_light_services.py
@@ -7,40 +7,51 @@ from svc.repository.models.lights import DeviceGroups, Devices
 from svc.services.light_service import get_light_groups, set_light_group
 
 
-@patch('svc.services.light_service.get_light_status')
+@patch('svc.services.light_service.tuya_utils')
 @patch('svc.services.light_service.LightDatabaseManager')
 class TestLightServices:
     SETTINGS = None
     GROUP_ID = str(uuid.uuid4())
-    DEVICE_ID = str(uuid.uuid4())
-    DB_DEVICE = Devices(id=DEVICE_ID, name='Lamp', ip_address='127.0.0.1', local_key='test', type_id='26cf7b35-8366-4c16-be9a-e0009bda62b6')
-    DB_GROUP = DeviceGroups(id=GROUP_ID, name='Bedroom', devices=[DB_DEVICE])
+    DEVICE_ONE_ID = str(uuid.uuid4())
+    DEVICE_TWO_ID = str(uuid.uuid4())
+    DEVICE_ONE = Devices(id=DEVICE_ONE_ID, name='Lamp', ip_address='127.0.0.1', local_key='test', type_id='26cf7b35-8366-4c16-be9a-e0009bda62b6')
+    DEVICE_TWO = Devices(id=DEVICE_TWO_ID, name='Lamp', ip_address='127.0.0.1', local_key='test', type_id='26cf7b35-8366-4c16-be9a-e0009bda62b6')
+    DB_GROUP = DeviceGroups(id=GROUP_ID, name='Bedroom', devices=[DEVICE_ONE])
     FILE_NAME = 'test.json'
 
     def setup_method(self):
         self.SETTINGS = Settings.get_instance()
         self.SETTINGS.dev_mode = True
 
-    def test_get_light_groups__should_call_db_for_groups(self, mock_db, mock_status):
+    def test_get_light_groups__should_call_db_for_groups(self, mock_db, mock_tuya):
         mock_db.return_value.__enter__.return_value.get_light_groups.return_value = [self.DB_GROUP]
 
         get_light_groups()
 
         mock_db.return_value.__enter__.return_value.get_light_groups.assert_called()
 
-    def test_get_light_groups__should_map_db_response(self, mock_db, mock_status):
-        light = {'name': 'Lamp', 'id': self.DEVICE_ID}
+    def test_get_light_groups__should_map_db_response(self, mock_db, mock_tuya):
+        light = {'name': 'Lamp', 'id': self.DEVICE_ONE_ID}
         expected = [{'groupName': 'Bedroom', 'groupId': self.GROUP_ID, 'lights': [light]}]
-        mock_status.return_value = light
+        mock_tuya.get_light_status.return_value = light
         mock_db.return_value.__enter__.return_value.get_light_groups.return_value = [self.DB_GROUP]
 
         actual = get_light_groups()
 
         assert actual == expected
 
-    def test_set_light_groups__should_get_groups_from_db(self, mock_db, mock_status):
+    def test_set_light_groups__should_get_groups_from_db(self, mock_db, mock_tuya):
         request = {'groupId': 'abc134', 'on': True}
-        mock_db.return_value.__enter__.return_value.get_light_groups.return_value = [self.DB_GROUP]
+        mock_db.return_value.__enter__.return_value.get_lights_by.return_value = [self.DB_GROUP]
         set_light_group(request)
 
-        mock_db.return_value.__enter__.return_value.get_light_groups.assert_called()
+        mock_db.return_value.__enter__.return_value.get_lights_by.assert_called()
+
+    def test_set_light_groups__should_set_state_of_all_lights_in_group(self, mock_db, mock_tuya):
+        request = {'groupId': 'abc134', 'on': True, 'brightness': 50}
+        mock_db.return_value.__enter__.return_value.get_lights_by.return_value = [self.DEVICE_ONE, self.DEVICE_TWO]
+
+        set_light_group(request)
+
+        mock_tuya.set_switch.assert_any_call(self.DEVICE_ONE, request['on'], request['brightness'])
+        mock_tuya.set_switch.assert_any_call(self.DEVICE_TWO, request['on'], request['brightness'])

5a83f1aa6f4c73921b6f940df244a90b099964f4
Jon Graf <jongraf1@gmail.com>
2023-03-31 19:47:10 -0500

    cleaned up no longer necessary test items

--- tests/unit/services/test_light_services.py
+++ tests/unit/services/test_light_services.py
@@ -2,7 +2,6 @@ import uuid
 
 from mock import patch
 
-from svc.config.settings_state import Settings
 from svc.repository.models.lights import DeviceGroups, Devices
 from svc.services.light_service import get_light_groups, set_light_group
 
@@ -10,18 +9,12 @@ from svc.services.light_service import get_light_groups, set_light_group
 @patch('svc.services.light_service.tuya_utils')
 @patch('svc.services.light_service.LightDatabaseManager')
 class TestLightServices:
-    SETTINGS = None
     GROUP_ID = str(uuid.uuid4())
     DEVICE_ONE_ID = str(uuid.uuid4())
     DEVICE_TWO_ID = str(uuid.uuid4())
     DEVICE_ONE = Devices(id=DEVICE_ONE_ID, name='Lamp', ip_address='127.0.0.1', local_key='test', type_id='26cf7b35-8366-4c16-be9a-e0009bda62b6')
     DEVICE_TWO = Devices(id=DEVICE_TWO_ID, name='Lamp', ip_address='127.0.0.1', local_key='test', type_id='26cf7b35-8366-4c16-be9a-e0009bda62b6')
     DB_GROUP = DeviceGroups(id=GROUP_ID, name='Bedroom', devices=[DEVICE_ONE])
-    FILE_NAME = 'test.json'
-
-    def setup_method(self):
-        self.SETTINGS = Settings.get_instance()
-        self.SETTINGS.dev_mode = True
 
     def test_get_light_groups__should_call_db_for_groups(self, mock_db, mock_tuya):
         mock_db.return_value.__enter__.return_value.get_light_groups.return_value = [self.DB_GROUP]

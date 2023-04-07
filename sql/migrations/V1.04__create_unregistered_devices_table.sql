1470f3ed28c8038869c07bc8b919c27cff8fb39c
Jon Graf <jgraf@vermeer.com>
2023-04-07 11:17:37 -0500

    turned off device type for unregistered devices as not sure we will have it

--- sql/migrations/V1.04__create_unregistered_devices_table.sql
+++ sql/migrations/V1.04__create_unregistered_devices_table.sql
@@ -3,6 +3,6 @@ CREATE TABLE unregistered_devices (
     name VARCHAR(40) NOT NULL,
     ip_address INET NOT NULL,
     local_key VARCHAR(40) NOT NULL,
-    device_id VARCHAR(40) NOT NULL,
-    type_id UUID REFERENCES device_types(id) NOT NULL
+    device_id VARCHAR(40) NOT NULL
+--     type_id UUID REFERENCES device_types(id) NOT NULL
 );

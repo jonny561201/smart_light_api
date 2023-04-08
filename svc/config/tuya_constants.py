72b779d0398848eca4c297e4ae460b3b8b9008b3
Jon Graf <jongraf1@gmail.com>
2023-04-07 19:40:26 -0500

    added constants for the tuya device types for creating new records

--- svc/config/tuya_constants.py
+++ svc/config/tuya_constants.py
@@ -7,5 +7,12 @@ class DeviceStates:
     COUNT_DOWN = '6'
 
 
+class TuyaTypes:
+    OUTLET = '26cf7b35-8366-4c16-be9a-e0009bda62b6'
+    COVER = '58927435-1afd-41b2-be1c-0ee21a8294b8'
+    BULB = '8a1e98ae-63c7-444e-971f-1781cc6f9352'
+    CLOUD = 'e60ae72e-4f16-40ff-a3cc-323effa73810'
+
+
 class GHOME:
     VERSION = 3.3

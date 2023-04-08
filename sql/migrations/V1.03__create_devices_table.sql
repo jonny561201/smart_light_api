e62130be43986f7a222811d8967355ecea891108
Jon Graf <jongraf1@gmail.com>
2023-04-07 19:12:54 -0500

    simplified migrations as probably dont need a separate primary keey

--- sql/migrations/V1.03__create_devices_table.sql
+++ sql/migrations/V1.03__create_devices_table.sql
@@ -1,9 +1,8 @@
 CREATE TABLE devices (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id VARCHAR(40) PRIMARY KEY NOT NULL,
     name VARCHAR(40) NOT NULL,
     ip_address INET NOT NULL,
     local_key VARCHAR(40) NOT NULL,
-    device_id VARCHAR(40) NOT NULL,
     type_id UUID REFERENCES device_types(id) NOT NULL,
     group_id UUID REFERENCES device_groups(id)
 );
--- sql/migrations/V1.04__create_unregistered_devices_table.sql
+++ sql/migrations/V1.04__create_unregistered_devices_table.sql
@@ -1,8 +1,7 @@
 CREATE TABLE unregistered_devices (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id VARCHAR(40) PRIMARY KEY,
     name VARCHAR(40) NOT NULL,
     ip_address INET NOT NULL,
-    local_key VARCHAR(40) NOT NULL,
-    device_id VARCHAR(40) NOT NULL
+    local_key VARCHAR(40) NOT NULL
 --     type_id UUID REFERENCES device_types(id) NOT NULL
 );

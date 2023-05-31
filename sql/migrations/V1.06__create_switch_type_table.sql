f94ea3fd01db5f6df8209c9852aec229f161fbd5
Jon Graf <jgraf@vermeer.com>
2023-05-30 22:44:22 -0500

    added sql migration for the switch type

--- sql/migrations/V1.06__create_switch_type_table.sql
+++ sql/migrations/V1.06__create_switch_type_table.sql
@@ -6,3 +6,8 @@ CREATE TABLE switch_type (
 INSERT INTO switch_type (id, name)
     VALUES ('b195b325-767a-4ecb-b54f-9aafe756f70b', 'light'),
            ('199a8196-0dd1-461e-adca-823da8e0b804', 'fan');
+
+ALTER TABLE devices ADD COLUMN switch_type_id UUID;
+
+ALTER TABLE devices ADD CONSTRAINT fk_switch_type FOREIGN KEY (switch_type_id)
+          REFERENCES switch_type (id);

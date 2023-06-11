8477ab9f139e23d71367833f33b8827885b01449
Jon Graf <jgraf@vermeer.com>
2023-06-10 22:58:08 -0500

    updated light routes to grab api key and pass it down to service layer

--- svc/routes/light_routes.py
+++ svc/routes/light_routes.py
@@ -10,54 +10,67 @@ DEFAULT_HEADERS = {'Content-Type': 'text/json'}
 
 @LIGHT_BLUEPRINT.route('/light/state', methods=['POST'])
 def set_light_state():
+    api_key = request.headers.get('LightApiKey')
     data = json.loads(request.data.decode('UTF-8'))
-    light_service.update_light_state(data)
+
+    light_service.update_light_state(api_key, data)
     return Response(status=200, headers=DEFAULT_HEADERS)
 
 
 @LIGHT_BLUEPRINT.route('/groups', methods=['GET'])
 def get_all_light_groups():
-    content = light_service.get_light_groups()
+    api_key = request.headers.get('LightApiKey')
+    content = light_service.get_light_groups(api_key)
     return Response(json.dumps(content), status=200, headers=DEFAULT_HEADERS)
 
 
 @LIGHT_BLUEPRINT.route('/group/state', methods=['POST'])
 def set_light_group():
+    api_key = request.headers.get('LightApiKey')
     data = json.loads(request.data.decode('UTF-8'))
-    light_service.set_light_group(data)
+
+    light_service.set_light_group(api_key, data)
     return Response(status=200, headers=DEFAULT_HEADERS)
 
 
 @LIGHT_BLUEPRINT.route('/group/create', methods=['POST'])
 def create_new_group():
+    api_key = request.headers.get('LightApiKey')
     data = json.loads(request.data.decode('UTF-8'))
-    group_id = light_service.create_group(data)
+
+    group_id = light_service.create_group(api_key, data)
     return Response(json.dumps(group_id), status=200, headers=DEFAULT_HEADERS)
 
 
 @LIGHT_BLUEPRINT.route('/group/<group_id>', methods=['DELETE'])
 def delete_group_by_id(group_id):
-    light_service.delete_group(group_id)
+    api_key = request.headers.get('LightApiKey')
+    light_service.delete_group(api_key, group_id)
     return Response(status=200, headers=DEFAULT_HEADERS)
 
 
 @LIGHT_BLUEPRINT.route('/unregistered', methods=['GET'])
 def get_unregistered_lights():
-    content = light_service.get_unregistered_devices()
+    api_key = request.headers.get('LightApiKey')
+    content = light_service.get_unregistered_devices(api_key)
     return Response(json.dumps(content), status=200, headers=DEFAULT_HEADERS)
 
 
 @LIGHT_BLUEPRINT.route('/group/assign', methods=['POST'])
 def assign_light_group():
+    api_key = request.headers.get('LightApiKey')
     data = json.loads(request.data.decode('UTF-8'))
-    light_service.assign_group(data)
+
+    light_service.assign_group(api_key, data)
     return Response(status=200, headers=DEFAULT_HEADERS)
 
 
-@LIGHT_BLUEPRINT.route('/group/light/update', methods=['POST'])
+@LIGHT_BLUEPRINT.route('/group/update', methods=['POST'])
 def update_light_group():
+    api_key = request.headers.get('LightApiKey')
     data = json.loads(request.data.decode('UTF-8'))
-    light_service.update_group(data)
+
+    light_service.update_group(api_key, data)
     return Response(status=200, headers=DEFAULT_HEADERS)
 
 # TODO: distinguish between switch/dimmer
--- tests/integration/test_light_routes.py
+++ tests/integration/test_light_routes.py
@@ -10,6 +10,7 @@ from svc.manager import app
 class TestLightRoutes:
     CONTENT = [{'test': 'fake'}]
     TEST_CLIENT = None
+    API_KEY = str(uuid.uuid4())
 
     def setup_method(self):
         self.TEST_CLIENT = app.test_client()
@@ -17,7 +18,7 @@ class TestLightRoutes:
     def test_get_all_light_groups__should_return_value_from_service(self, mock_service):
         mock_service.get_light_groups.return_value = self.CONTENT
 
-        actual = self.TEST_CLIENT.get('/lights/groups')
+        actual = self.TEST_CLIENT.get('/api/lights/groups')
 
         assert actual.status_code == 200
         assert json.loads(actual.data) == self.CONTENT
@@ -25,61 +26,61 @@ class TestLightRoutes:
     def test_set_light_state__should_return_success_from_service(self, mock_service):
         post_body = {'fake': 'dont matter'}
 
-        actual = self.TEST_CLIENT.post('/lights/light/state', data=json.dumps(post_body))
+        actual = self.TEST_CLIENT.post('/api/lights/light/state', data=json.dumps(post_body))
 
         assert actual.status_code == 200
 
     def test_set_light_state__should_call_light_service_with_request(self, mock_service):
         body = {'groupId': 'test'}
 
-        self.TEST_CLIENT.post('/lights/light/state', data=json.dumps(body))
+        self.TEST_CLIENT.post('/api/lights/light/state', data=json.dumps(body))
 
         assert mock_service.update_light_state(body)
 
     def test_set_light_group__should_return_success_from_service(self, mock_service):
         body = {'groupId': 'test'}
 
-        actual = self.TEST_CLIENT.post('/lights/group/state', data=json.dumps(body))
+        actual = self.TEST_CLIENT.post('/api/lights/group/state', data=json.dumps(body))
 
         assert actual.status_code == 200
 
     def test_set_light_group__should_call_light_service_with_request(self, mock_service):
         body = {'groupId': 'test'}
 
-        self.TEST_CLIENT.post('/lights/group/state', data=json.dumps(body))
+        self.TEST_CLIENT.post('/api/lights/group/state', data=json.dumps(body), headers={'LightApiKey': self.API_KEY})
 
-        mock_service.set_light_group.assert_called_with(body)
+        mock_service.set_light_group.assert_called_with(self.API_KEY, body)
 
     def test_create_light_group__should_return_id_from_service(self, mock_service):
         body = {'name': 'doesnt matter'}
         group_id = str(uuid.uuid4())
         mock_service.create_group.return_value = group_id
 
-        actual = self.TEST_CLIENT.post('/lights/group/create', data=json.dumps(body))
+        actual = self.TEST_CLIENT.post('/api/lights/group/create', data=json.dumps(body))
 
         actual.status_code = 200
         assert json.loads(actual.data) == group_id
 
     def test_delete_group_by_id__should_call_service(self, mock_service):
         group_id = str(uuid.uuid4())
-        self.TEST_CLIENT.delete(f'/lights/group/{group_id}')
+        self.TEST_CLIENT.delete(f'/api/lights/group/{group_id}', headers={'LightApiKey': self.API_KEY})
 
-        mock_service.delete_group.assert_called_with(group_id)
+        mock_service.delete_group.assert_called_with(self.API_KEY, group_id)
 
     def test_delete_group_by_id__should_return_success(self, mock_service):
         group_id = str(uuid.uuid4())
-        actual = self.TEST_CLIENT.delete(f'/lights/group/{group_id}')
+        actual = self.TEST_CLIENT.delete(f'/api/lights/group/{group_id}')
 
         assert actual.status_code == 200
 
     def test_update_light_group__should_return_success(self, mock_service):
         request = {'groupId': str(uuid.uuid4()), 'lightId': str(uuid.uuid4())}
-        actual = self.TEST_CLIENT.post('/lights/group/light/update', data=json.dumps(request))
+        actual = self.TEST_CLIENT.post('/api/lights/group/update', data=json.dumps(request))
 
         assert actual.status_code == 200
 
     def test_update_light_group__should_call_service_layer_with_request(self, mock_service):
         request = {'groupId': str(uuid.uuid4()), 'lightId': str(uuid.uuid4())}
-        self.TEST_CLIENT.post('/lights/group/update', data=json.dumps(request))
+        self.TEST_CLIENT.post('/api/lights/group/update', data=json.dumps(request), headers={'LightApiKey': self.API_KEY})
 
-        mock_service.update_group.assert_called_with(request)
+        mock_service.update_group.assert_called_with(self.API_KEY, request)
